5c3cc54a23b9ed85704a79c67ddb32dfb5665ab9
Tkunl <tkunl@foxmail.com>
2025-04-29 22:59:34 +0800

    feat: 整理了一下依赖

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config';
+import { projectConfig } from './core/config/project.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [projectConfig],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
